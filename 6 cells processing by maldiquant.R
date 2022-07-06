d6df090d99170666dc1024f31f0ce165bd2fdef8
jarquinsandra <jarquinsandra@gmail.com>
2022-07-06 12:28:19 +0100

    add 4 digits round

--- 6 cells processing by maldiquant.R
+++ 6 cells processing by maldiquant.R
@@ -18,20 +18,25 @@ library("ggpubr")
 library(RVAideMemoire)
 
 ## get names of all .mzML files in current working directory
-path <- "~/Sandra_MS_data/maldiquant_cells/neg_raw"
+path <- "~/Sandra_MS_data/maldiquant_cells/pos_raw/raw"
 spectra<-importMzMl(path = path)
 #normalization to TIC
 
 spectra <- calibrateIntensity(spectra, method="TIC")
 
 ## Peak picking, you can change SNR however for orbitrap data the noise is very low
-
 peaks<-detectPeaks(spectra, SNR=3)
 ## Peak alignment using the warping function of Maldiquant
 
-warpingFunctions<-determineWarpingFunctions(peaks)
+warpingFunctions<-determineWarpingFunctions(peaks,allowNoMatches=TRUE)
 peaks<-warpMassPeaks(peaks,warpingFunctions)
 
+# binPeaks tolerance is expressed in delta mass / mass, we want it in ppm:
+#tol.ppm <- 5
+#tol.maldiquant <- tol.ppm / 1e5
+
+# The new common masses will overwrite the original ones. The intensities remain unmodified
+#peaks <- binPeaks(spectra, method = 'strict', tolerance = tol.ppm)
 #Peak binning and creation of feature matrix
 peaks<-binPeaks(peaks)
 featureMatrix<-intensityMatrix(peaks)
@@ -47,20 +52,39 @@ features<-as_tibble(featureMatrix,.name_repair = ~ make.names(., unique = TRUE))
 zerocutoff<-nrow(features)*0.2
 
 features[colSums(features > 0) <= zerocutoff]<- NULL
-apply( features , 2 , function(x) sum ( x == 0 ) )
+#apply( features , 2 , function(x) sum ( x == 0 ) )
 
-#select all features in original tibble
-features<-features%>% select(all_of(ft))
-#1negative 
-#spectra[[1998]]@metaData[["file"]]
-ASPC<-replicate(1268, "ASPC")
-BxPC<-replicate(1264, "BxPC")
-CFPAC<-replicate(1337, "CFPAC")
-HeLa<-replicate(1264, "HeLa")
-Panc1<-replicate(1484, "Panc1")
-Panc10<-replicate(1267, "Panc10")
-
-alllabels<-c(ASPC,BxPC,CFPAC,HeLa,Panc1,Panc10)
+# #1negative 
+# #spectra[[1998]]@metaData[["file"]]
+ #ASPC<-replicate(1268, "ASPC")
+ #BxPC<-replicate(1264, "BxPC")
+ #CFPAC<-replicate(1337, "CFPAC")
+ #HeLa<-replicate(1264, "HeLa")
+ #Panc1<-replicate(1484, "Panc1")
+ #Panc10<-replicate(1267, "Panc10")
+# 
+ #alllabels<-c(ASPC,BxPC,CFPAC,HeLa,Panc1,Panc10)
+# #1positive 
+# spectra[[1998]]@metaData[["file"]]
+ ASPC<-replicate(1265, "ASPC")
+ BxPC<-replicate(1065, "BxPC")
+ CFPAC<-replicate(1265, "CFPAC")
+ HeLa<-replicate(1263, "HeLa")
+ Panc1<-replicate(1265, "Panc1")
+ Panc10<-replicate(1262, "Panc10")
+ alllabels<-c(ASPC,BxPC,CFPAC,HeLa,Panc1,Panc10)
+ 
+# 
+ 
+ # #cancer markers
+ # #spectra[[1998]]@metaData[["file"]]
+ #ASPC<-replicate(1265, "PC")
+ #BxPC<-replicate(1065, "BxPC")
+ #CFPAC<-replicate(1265, "PC")
+ #HeLa<-replicate(1263, "HeLa")
+ #Panc<-replicate(2527, "PC")
+ #alllabels<-c(ASPC,BxPC,CFPAC,HeLa,Panc)
+ 
 #add labels
 features$sample<-alllabels
 features<-tibble::rowid_to_column(features, "id")
@@ -69,7 +93,10 @@ features <- features %>%
   select(id,sample, everything())
 
 #filter rows by mass to select cells Negative
-cells<-features[!(features$X804.574332386052==0|features$X830.606791452377)==0,]
+#cells<-features[!(features$X804.574332386052==0|features$X830.606791452377)==0,]
+#filter rows by mass to select cells positive
+
+cells<-features[!(features$X732.55685675744==0|features$X760.589528926264)==0,]
 
 
 
@@ -77,7 +104,7 @@ library(randomForest)
 require(caTools)
 library(caret)
 set.seed(222)
-rfdata<-features[,2:115]
+rfdata<-cells[,2:405]
 rfdata$sample<-as_factor(rfdata$sample)
 ind <- sample(2, nrow(rfdata), replace = TRUE, prob = c(0.7, 0.3))
 train <- rfdata[ind==1,]
@@ -123,17 +150,17 @@ feat_imp_df <- importance(rf) %>%
 library(Rtsne)
 
 ## Curating the database for analysis with both t-SNE and PCA
-Labels<-features$sample
-features$sample<-as.factor(features$sample)
+Labels<-cells$sample
+cells$sample<-as.factor(cells$sample)
 ## for plotting
-colors = rainbow(length(unique(features$sample)))
-names(colors) = unique(features$sample)
+colors = rainbow(length(unique(cells$sample)))
+names(colors) = unique(cells$sample)
 
 ## Executing the algorithm on curated data
-tsne <- Rtsne(features[,3:115], dims = 2, perplexity=100, verbose=TRUE, max_iter = 1000)
+tsne <- Rtsne(cells[,3:ncol(features)], dims = 2, perplexity=100, verbose=TRUE, max_iter = 1000)
 #get labels
-spectranumcol<-features$id
-samplecol<-features$sample
+spectranumcol<-cells$id
+samplecol<-cells$sample
 tSNE_df <- tsne$Y %>% 
   as.data.frame() %>%
   dplyr::rename(tsne1="V1",
@@ -171,18 +198,18 @@ class(boruta.df)
 
 
 
-features<-features %>% 
+cells<-cells %>% 
   mutate_if(is.numeric, round,digits=4)
 
 #transpose the matrix including column names as a column called featur
-trans_test2<- data.table::transpose(features,keep.names = "feature")
+trans_test2<- data.table::transpose(cells,keep.names = "feature")
 trans_test2<-trans_test2[-2,]
 trans_test2<-as_tibble(trans_test2)
 
 ####Get selected features from the table
 keep<- c("sample", "id", selected_features)
 
-df <- features[keep]
+df <- cells[keep]
 
 #get only confirmed features
 boruta.df<-filter(boruta.df, decision=='Confirmed')
@@ -190,13 +217,13 @@ mz<-rownames(boruta.df)
 boruta.df$mz<-mz
 boruta.df$mz<-gsub("X","",boruta.df$mz)
 boruta.df$mz<-as.numeric(boruta.df$mz)
+boruta.df$mz<-round(boruta.df$mz,digits=4)
 boruta.df<-as_tibble(boruta.df)
 write.csv(boruta.df, file='featuresimportance.csv') 
 
-####clases file from python
-names<-colnames(clusters)
+names<-colnames(df)
 names<-sub('X', '', names)
-names<-names[3:161]
+names<-names[3:ncol(df)]
 names<-as.numeric(names)
 names<-round(names,digits=4)
 names<-as.character(names)
@@ -204,7 +231,10 @@ names<-as.character(names)
 #add X to characters
 names<-paste0("X", names)
 labels<-c("sample","id",names,"hdb_prob", "hdb_cluster")
-colnames(clusters)<-labels
+colnames(df)<-labels
+write.csv(df, file='cellsborutaselectedfeatures_filtered.csv') 
+
+
 
 
 
